c26ce89873ee0f08251c06ddf85f12a95eaa73eb
Russel E Tagaca <russeltagaca@gmail.com>
2020-12-11 20:02:05 -0500

    Add files via upload

--- jqAJAX.php
+++ jqAJAX.php
@@ -3,7 +3,7 @@
 $zip = $_GET["zip"];
 
 
-$request = "http://api.openweathermap.org/data/2.5/weather?zip=$zip,us&appid=66b77681f51a6f40fb7198eed9293758";
+$request = "https://api.openweathermap.org/data/2.5/weather?zip=$zip,us&appid=dbd3b02d8958d62185d02e944cd5f522";
 $jsonWeather  = file_get_contents($request);
 print "$jsonWeather";
 ?>
