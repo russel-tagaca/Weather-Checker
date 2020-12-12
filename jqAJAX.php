03d10ba7189ca2c89263c5f572bb4a42e88f1d83
Russel E Tagaca <russel.tagaca@gmail.com>
2020-12-11 21:30:06 -0500

    Update jqAJAX.php

--- jqAJAX.php
+++ jqAJAX.php
@@ -3,7 +3,7 @@
 $zip = $_GET["zip"];
 
 
-$request = "https://api.openweathermap.org/data/2.5/weather?zip=$zip,us&appid=dbd3b02d8958d62185d02e944cd5f522";
+$request = "https://api.openweathermap.org/data/2.5/weather?zip=$zip,us&appid={?}";
 $jsonWeather  = file_get_contents($request);
 print "$jsonWeather";
 ?>
